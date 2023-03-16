18145bd40c5863757d62ab98709188f1a7be7a1c
3NinjasFun <739237@student.dusd.net>
2023-03-16 09:46:40 -0700

    Finished alll chalenmges
    Im so Smart

--- Unit2SanchezP1/Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Unit2SanchezP1/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -5,13 +5,17 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
+    float timer;
+    public float spawnDelay = 1f;
 
     // Update is called once per frame
     void Update()
     {
+        timer += Time.deltaTime;
         // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && timer > spawnDelay)
         {
+            timer = 0f;
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
         }
     }
--- Unit2SanchezP1/Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Unit2SanchezP1/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -12,23 +12,26 @@ public class SpawnManagerX : MonoBehaviour
 
     private float startDelay = 1.0f;
     public float spawnInterval = 3.0f;
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        SpawnRandomBall ();
+        Invoke("SpawnRandomBall", startDelay);
     }
 
     // Spawn random ball at random x position at top of play area
     void SpawnRandomBall ()
     {
         // Generate random ball index and random spawn position
+        int ballIndex = Random.Range(0, ballPrefabs.Length);
 
         Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
 
         // instantiate ball at random spawn location and random intervals
-        int ballIndex = Random.Range(0, ballPrefabs.Length);
-        Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[0].transform.rotation);
-        Invoke("SpawnRandomball", Random.Range(2f,5f));
+        
+        Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[ballIndex].transform.rotation);
+        Invoke("SpawnRandomBall", Random.Range(2, 5));
+        
     }
 }
